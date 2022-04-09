ec1353dcd30e27d2769cb438f9cebab067987eea
shri142 <shrikrishna2002umbare@gmail.com>
2022-04-09 14:37:12 +0530

    Update logic.py

--- src/logic.py
+++ src/logic.py
@@ -1,90 +1,93 @@
-# imports
+# # imports
 
-# caesars' cipher function
+# # caesars' cipher function
 
-# encryption
+# # encryption
 
-def encryption(message, shifter_key):
-    encryption_result = ""
+# def encryption(message, shifter_key):
+#     encryption_result = ""
 
-    # go through every character
-    for i in range(len(message)):
-        letter = message[i]
+#     # go through every character
+#     for i in range(len(message)):
+#         letter = message[i]
 
-        if (letter.isupper()):
-            encryption_result += chr((ord(letter) + shifter_key - 65)%26 +65)
-        else:
-            encryption_result += chr((ord(letter) + shifter_key - 97)%26 +97)
+#         if (letter.isupper()):
+#             encryption_result += chr((ord(letter) + shifter_key - 65)%26 +65)
+#         else:
+#             encryption_result += chr((ord(letter) + shifter_key - 97)%26 +97)
 
-    return encryption_result
+#     return encryption_result
 
-# move the text
-
-
-def encrypt_rot13(message, key=13):
-    encryption_result = ""
-
-    for i in range(len(message)):
-        letter = message[i]
-        if (letter.isalpha()):
-            if (letter.isupper()):
-                encryption_result += chr((ord(letter) - key - 65)%26 +65)
-            elif (letter.islower()):
-                encryption_result += chr((ord(letter) - key - 97)%26 +97)
-        elif (letter.isnumeric()):
-            encryption_result += letter
-        elif (letter == " "):
-            encryption_result += letter
-        else:
-            if (letter == "."):
-                encryption_result += letter
-    return encryption_result
-
-print(encrypt_rot13("o"))
+# # move the text
 
 
-import tkinter
-from tkinter import ttk
+# def encrypt_rot13(message, key=13):
+#     encryption_result = ""
 
-main_window = tkinter.Tk()
+#     for i in range(len(message)):
+#         letter = message[i]
+#         if (letter.isalpha()):
+#             if (letter.isupper()):
+#                 encryption_result += chr((ord(letter) - key - 65)%26 +65)
+#             elif (letter.islower()):
+#                 encryption_result += chr((ord(letter) - key - 97)%26 +97)
+#         elif (letter.isnumeric()):
+#             encryption_result += letter
+#         elif (letter == " "):
+#             encryption_result += letter
+#         else:
+#             if (letter == "."):
+#                 encryption_result += letter
+#     return encryption_result
 
-main_window.geometry("800x800")
+# print(encrypt_rot13("o"))
 
-new_combo_box = ttk.Combobox(values = [
-    "David",
-    "Alex"
-])
-new_combo_box.grid(row = 0, column = 0)
-new_combo_box.current(1)
 
-def get_combo_value():
-    the_value = new_combo_box.get()
-    return the_value
+# import tkinter
+# from tkinter import ttk
 
-selected = get_combo_value()
+# main_window = tkinter.Tk()
 
-execute_button = tkinter.Button(text = "select", command = get_combo_value)
-execute_button.grid(row = 1, column = 0)
+# main_window.geometry("800x800")
 
-change_label = tkinter.Label(text = selected)
-change_label.grid(row = 2, column = 0)
-change_label.update()
+# new_combo_box = ttk.Combobox(values = [
+#     "David",
+#     "Alex"
+# ])
+# new_combo_box.grid(row = 0, column = 0)
+# new_combo_box.current(1)
 
-print(selected)
+# def get_combo_value():
+#     the_value = new_combo_box.get()
+#     return the_value
 
-main_window.mainloop()
+# selected = get_combo_value()
+
+# execute_button = tkinter.Button(text = "select", command = get_combo_value)
+# execute_button.grid(row = 1, column = 0)
+
+# change_label = tkinter.Label(text = selected)
+# change_label.grid(row = 2, column = 0)
+# change_label.update()
+
+# print(selected)
+
+# main_window.mainloop()
+
 
 # vigenere cipher
 
 """
-alphabet = a b c d e f g h 
-keys     = 1 2 3 4 5 6 7 9 
-keyword = "bad" 
+alphabet = a b c d e f g h
+keys     = 1 2 3 4 5 6 7 9
+
+keyword = "bad"
            214
 plain_text        =     "adefhcba"
                          badbadba
 shift_by                 21421421
 encrypted_message =      ceihigdb
+
 """
 from string import ascii_letters
 
