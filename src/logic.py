d24ab89c881bf9cc445f7abd9216bcef77a46e9a
shri142 <shrikrishna2002umbare@gmail.com>
2022-04-09 14:03:42 +0530

    Update logic.py

--- src/logic.py
+++ src/logic.py
@@ -18,3 +18,26 @@ def encryption(message, shifter_key):
 
     return encryption_result
 
+# move the text
+
+
+def encrypt_rot13(message, key=13):
+    encryption_result = ""
+
+    for i in range(len(message)):
+        letter = message[i]
+        if (letter.isalpha()):
+            if (letter.isupper()):
+                encryption_result += chr((ord(letter) - key - 65)%26 +65)
+            elif (letter.islower()):
+                encryption_result += chr((ord(letter) - key - 97)%26 +97)
+        elif (letter.isnumeric()):
+            encryption_result += letter
+        elif (letter == " "):
+            encryption_result += letter
+        else:
+            if (letter == "."):
+                encryption_result += letter
+    return encryption_result
+
+print(encrypt_rot13("o"))
