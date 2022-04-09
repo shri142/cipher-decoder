d838309e9f5c6be23ad7fedae490e6cf908e1956
shri142 <shrikrishna2002umbare@gmail.com>
2022-04-09 13:58:10 +0530

    encryption decryption function added

--- src/main.py
+++ src/main.py
@@ -11,7 +11,7 @@ class MainWindow(tkinter.Tk):
         super().__init__()
         """ The main window properties """
         # main window title
-        self.title("Cryptographic Ciphers")
+        self.title("Cipher decoder")
         # main window size
         self.geometry("1320x420")
         # main window color
@@ -38,6 +38,67 @@ class MainWindow(tkinter.Tk):
                     encryption_result += "."
 
         return encryption_result
+
+        # decryption function
+
+    def decryption(self, message, shifter_key):
+        decryption_result = ""
+
+        # go through every character
+        for i in range(len(message)):
+            letter = message[i]
+            if (letter.isalpha()):
+                if (letter.isupper()):
+                    decryption_result += chr((ord(letter) - shifter_key - 65) % 26 + 65)
+                elif (letter.islower()):
+                    decryption_result += chr((ord(letter) - shifter_key - 97) % 26 + 97)
+            elif (letter.isnumeric()):
+                decryption_result += letter
+            else:
+                if (letter == " "):
+                    decryption_result += " "
+                if (letter == "."):
+                    decryption_result += letter
+
+        return decryption_result
+
+    def encrypt_rot13(self, message, key=13):
+        encryption_result = ""
+
+        for i in range(len(message)):
+            letter = message[i]
+            if (letter.isalpha()):
+                if (letter.isupper()):
+                    encryption_result += chr((ord(letter) + key - 65) % 26 + 65)
+                elif (letter.islower()):
+                    encryption_result += chr((ord(letter) + key - 97) % 26 + 97)
+            elif (letter.isnumeric()):
+                encryption_result += letter
+            elif (letter == " "):
+                encryption_result += letter
+            else:
+                if (letter == "."):
+                    encryption_result += letter
+        return encryption_result
+
+    def decrypt_rot13(self, message, key=13):
+        decryption_result = ""
+
+        for i in range(len(message)):
+            letter = message[i]
+            if (letter.isalpha()):
+                if (letter.isupper()):
+                    decryption_result += chr((ord(letter) - key - 65) % 26 + 65)
+                elif (letter.islower()):
+                    decryption_result += chr((ord(letter) - key - 97) % 26 + 97)
+            elif (letter.isnumeric()):
+                decryption_result += letter
+            elif (letter == " "):
+                decryption_result += letter
+            else:
+                if (letter == "."):
+                    decryption_result += letter
+        return decryption_result
 
 
 
