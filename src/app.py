76a981160c11040a24320dc99cc5beaf72e628a7
shri142 <shrikrishna2002umbare@gmail.com>
2022-04-09 14:09:23 +0530

    Update app.py

--- src/app.py
+++ src/app.py
@@ -195,3 +195,45 @@ def move_text_to_decrypt_box():
 
     placeholder_label = mn.tkinter.Label(text="                      ", font=("times", 20), bg="light blue")
     placeholder_label.grid(row=0, column=2)
+
+    # encryption_message
+    encryption_message = mn.tkinter.Label(text="Encrypt", font=("times", 17), pady=20, bg="light blue")
+    encryption_message.grid(row=1, column=0, sticky=mn.tkinter.W)
+
+    # decryption_message
+    decryption_message = mn.tkinter.Label(text="Decrypt", font=("times", 17), pady=20, bg="light blue")
+    decryption_message.grid(row=1, column=2, sticky=mn.tkinter.E)
+
+    # encryption_box
+    encryption_box = mn.tkinter.Text(height=12, width=50, font=("times", 16))
+    encryption_box.grid(row=2, column=0, sticky=mn.tkinter.W)
+
+    # shifter_key
+    shifter_key = mn.tkinter.Label(text="Shifter Key / Keyword", font=("times", 15), bg="light blue")
+    shifter_key.grid(row=1, column=1)
+
+    # shifter_key_box
+    shifter_key_box = mn.tkinter.Text(height=2, width=8, font=("times", 15))
+    shifter_key_box.grid(row=2, column=1)
+    shifter_key_box.insert(tkinter.END, "13")
+    # encryption_button
+    encryption_button = mn.tkinter.Button(text="                    ENCRYPT                     ",
+                                          font=("times", 15), pady=10, command=move_text_to_decrypt_box)
+    encryption_button.grid(row=3, column=0, sticky=mn.tkinter.W)
+
+    # decrption_box
+    decrption_box = mn.tkinter.Text(height=12, width=50, font=("times", 16))
+    decrption_box.grid(row=2, column=2, sticky=mn.tkinter.E)
+
+    # Decrption button
+    decryption_button = mn.tkinter.Button(text="                    DECRYPT                     ",
+                                          font=("times", 15), pady=10, command=move_text_to_encrypted_box)
+    decryption_button.grid(row=3, column=2, sticky=mn.tkinter.E)
+
+    # cipher chooser
+    cipher_choose_label = tkinter.Label(text="Choose cipher: ", font=("text", 15), bg="light blue")
+    cipher_choose_label.grid(row=4, column=0, sticky=mn.tkinter.W, pady=10)
+
+    # loop the program
+    if __name__ == "__main__":
+        mn.root_window.mainloop()
